e8355d684f36ec03aae05d268bb242bfe4a25705
Saikat Mitra <chatwithsaikat@gmail.com>
2023-09-18 15:02:34 +0530

    changes

---
+++ sum_of_two_numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two Numbers: ";
+    cin>>a>>b;
+    c = a + b;
+    cout<<"Sum of two number is: "<<c<<endl;
+    
+}
