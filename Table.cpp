822158ba5c6b25a58bccf2d93cc34344c33fc992
Saikat Mitra <chatwithsaikat@gmail.com>
2023-09-19 22:08:51 +0530

    Table.cpp

---
+++ Table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,a;
+    cout<<"Enter The Number";
+    cin>>n;
+    cout<<endl;
+    for(int i=1;i<=10;i++)
+    {
+       a= n*i;
+       cout<<a<<endl;
+    }
+}
