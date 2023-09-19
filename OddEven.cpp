23980ad3f49b91e7110dac34c087037d4a6acbea
Saikat Mitra <chatwithsaikat@gmail.com>
2023-09-19 23:24:16 +0530

    OddEven.cpp

---
+++ OddEven.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter The Number: ";
+    cin>>n;
+    if(n%2==1)
+    {
+        cout<<"Odd"<<endl;
+    }
+    else
+    {
+        cout<<"Even"<<endl;
+    }
+}
