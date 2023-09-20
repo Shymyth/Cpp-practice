749541bcde1b93ecb3a5ffd7b18c3acd569703c5
Saikat Mitra <chatwithsaikat@gmail.com>
2023-09-20 23:57:00 +0530

    SwappingtwoNumbers.cpp

---
+++ SwappingtwoNumbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter The Numbers: ";
+    cin>>a>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<endl<<"Swapping Value is: "<<a<<" "<<b<<endl;
+
+}
