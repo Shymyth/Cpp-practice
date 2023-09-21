b2296509d830e7250fd86000deb6d59127e93e04
Saikat Mitra <chatwithsaikat@gmail.com>
2023-09-21 23:43:21 +0530

    CheckAVowel.cpp

---
+++ CheckAVowel.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a;
+    cout<<"Enter a Character "<<endl;
+    cin>>a;
+    switch(a)
+    {
+        case 'a':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'A':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'e':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'E':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'i':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'I':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'o':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'O':
+            cout<<"Vowel"<<endl;
+            break;
+        case 'u':
+            cout<<"Vowel"<<endl;
+            break;
+            case 'U':
+            cout<<"Vowel"<<endl;
+            break;
+           default:
+            cout<<"Consonant"<<endl;
+            break;
+    }
+    
+}
